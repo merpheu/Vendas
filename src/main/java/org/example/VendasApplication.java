2537c4458cf1ad480e5bfec7a7841ad9c3749fd9
rafaelnascimento <rafael.nascimento@eldorado.org.br>
2023-10-31 16:18:25 -0300

    query methods

--- src/main/java/org/example/VendasApplication.java
+++ src/main/java/org/example/VendasApplication.java
@@ -4,51 +4,33 @@ package org.example;
 import org.example.domain.entity.Cliente;
 import org.example.domain.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 
 public class VendasApplication {
+    private final Clientes clientes;
+
+    public VendasApplication(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
             System.out.println("Salvando clientes");
-            clientes.salvar(new Cliente());
-            List<Cliente> todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
+            clientes.save(new Cliente());
+            clientes.save(new Cliente());
 
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado.");
-                clientes.atualizar(c);
-            });
+            boolean existe = clientes.existsByNome("xyz");
+            System.out.println("existe um cliente com o nome xyz? " + existe);
 
-            todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
 
-            System.out.println("Buscando clientes");
-            clientes.buscarPorNome("Cli").forEach(System.out::println);
-
-            System.out.println("deletando clientes");
-            clientes.obterTodos().forEach(c -> {
-                clientes.deletar(c);
-            });
-
-            todosClientes = clientes.obterTodos();
-            if(todosClientes.isEmpty()){
-                System.out.println("Nenhum cliente encontrado.");
-            }else{
-                todosClientes.forEach(System.out::println);
-            }
         };
     }
 
